8b4d39e52afde4347e5590063820aa9174ec49ed
Ken Henderson <kenhenderson12@hotmail.com>
2018-04-01 18:38:45 -0400

    Moved the Icons enumeration outside of the Helper class, and changed all instances of Helper.Icons to just Icons.

---
+++ ConferenceFormSubmittal/Utilities/Icons.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConferenceFormSubmittal.Utilities
+{
+    public enum Icons
+    {
+        Plus,
+        Minus,
+        Edit,
+        View,
+        Flag,
+        Submitted,
+        Draft,
+        Approved,
+        Denied,
+        Check,
+        Cancel,
+        Back,
+        Save,
+        Filter,
+        Create,
+        Eraser
+    }
+}
