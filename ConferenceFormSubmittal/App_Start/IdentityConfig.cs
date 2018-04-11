1a7b9efd1cc402b660c1688780742dadd361f424
Rhodri Mativo <rmativo3@ncstudents.niagaracollege.ca>
2018-04-11 03:26:08 -0400

    fixed build errors

--- ConferenceFormSubmittal/App_Start/IdentityConfig.cs
+++ ConferenceFormSubmittal/App_Start/IdentityConfig.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using ConferenceFormSubmittal.Models;
+using ConferenceFormSubmittal.DAL;
 
 namespace ConferenceFormSubmittal
 {
